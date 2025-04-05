9efea5cafff9e720a3ddcf197eb2f72d0adf3a41
panscan <joeavanzato@gmail.com>
2025-04-05 14:22:57 -0400

    Update socgholish_c2_decrypter.py
    Cleaning

--- socgholish_c2_decrypter.py
+++ socgholish_c2_decrypter.py
@@ -31,8 +31,6 @@ def main():
     data = read_file_to_string(filePath)
     if data is None:
         exit()
-
-    # now ewe have the initial payload, we want to basically feed it into run
     decrypted = run(data)
     with open("decrypted_source.txt", "w") as f:
         f.write(decrypted)
@@ -52,6 +50,19 @@ def run (data):
     tmp_data = data
     run_id = 0
     loc = {}
+    pattern_pbkdf = r'key\s*=\s*pbkdf2[a-zA-Z0-9_]+\('
+    replacement_pbkdf = "key = PBKDF2("
+    pattern_b85 = r'data\s*=\s*b85_[a-zA-Z0-9_]+\(enc\)'
+    replacement_b85 = "data = base64.b85decode(enc)"
+    pattern_zlib = r'data\s*=\s*decomp_[a-zA-Z0-9_]+\('
+    replacement_zlib = "data = zlib.decompress("
+    pattern_cha= r'cipher\s*=\s*chacha[a-zA-Z0-9_]+\('
+    replacement_cha = "cipher = ChaCha20.new("
+    pattern_aesnew = r'cipher\s*=\s*aesnew[a-zA-Z0-9_]+\('
+    replacement_aesnew = "cipher = AES.new("
+    pattern_decode = r'return\s*decode[a-zA-Z0-9_]+\(data\)'
+    replacement_decode  = "return data.decode('utf-8')"
+    
     for i in range(1000):
         # Some checks for strings typically in the final payload
         if "ConnectionTimeoutOccuredError" in tmp_data and "ConnectionRefusedError" in tmp_data:
@@ -63,43 +74,21 @@ def run (data):
         # May need to adjust this based on the payload in question
         tmp_data = tmp_data.replace("['vm', 'virtual']", "['nonsense']")
         tmp_data = tmp_data.replace("__debug__", "__nonsense__")
-        # Then we will take data and scan for a string like exec(pc_start('...'))
         # We want to avoid actually exec the code, just the decode piece, so we modify the file in-line
         tmp_data = tmp_data.replace("exec(", "returned=")
         tmp_data = tmp_data.replace("def launch_hidden():", "def launch_hidden2():") # Rename
         tmp_data = tmp_data.replace("launch_hidden()", "#launch_hidden()") # Comment out
-        #data = data.replace("'))", "')")
         tmp_data = rreplace(tmp_data, ')', '')
 
         # Now we want to replace the other random vars
-        # First look for string like: data = b85_*(enc), replace with base64.b85decode
-        pattern_b85 = r'data\s*=\s*b85_[a-zA-Z0-9_]+\(enc\)'
-        replacement_b85 = "data = base64.b85decode(enc)"
         tmp_data = re.sub(pattern_b85, replacement_b85, tmp_data, 1)
-
-        pattern_pbkdf = r'key\s*=\s*pbkdf2[a-zA-Z0-9_]+\('
-        replacement_pbkdf = "key = PBKDF2("
         tmp_data = re.sub(pattern_pbkdf, replacement_pbkdf, tmp_data, 1)
-
-        pattern_aesnew = r'cipher\s*=\s*aesnew[a-zA-Z0-9_]+\('
-        replacement_aesnew = "cipher = AES.new("
         tmp_data = re.sub(pattern_aesnew, replacement_aesnew, tmp_data, 1)
-
-        pattern_cha= r'cipher\s*=\s*chacha[a-zA-Z0-9_]+\('
-        replacement_cha = "cipher = ChaCha20.new("
         tmp_data = re.sub(pattern_cha, replacement_cha, tmp_data, 1)
-
-        pattern_zlib = r'data\s*=\s*decomp_[a-zA-Z0-9_]+\('
-        replacement_zlib = "data = zlib.decompress("
         tmp_data = re.sub(pattern_zlib, replacement_zlib, tmp_data, 1)
-
-        pattern_decode = r'return\s*decode[a-zA-Z0-9_]+\(data\)'
-        replacement_decode  = "return data.decode('utf-8')"
         tmp_data = re.sub(pattern_decode, replacement_decode, tmp_data, 1)
 
-        # Find and replace hw key for current run
-        # return b'0123456789ABCDEF0123456789ABCDEF'
-        # Find Key
+        # Find and replace hw key for current run - this algorithm may change in future payloads, we observed static before
         key_pattern = re.compile("return b'(?P<hw_key>[0-9A-Z]{30,35})'")
         matches = key_pattern.search(tmp_data)
         if matches is not None:
@@ -109,8 +98,8 @@ def run (data):
 
         print("###################################################################### CLEANED DATA")
         print(tmp_data)
-        exec_globals = globals().copy()  # Start with a copy of the current globals
-
+        # Probably could have passed globals() directly but had some other plans that I later on changed
+        exec_globals = globals().copy()
         exec(tmp_data, exec_globals, loc)
         tmp_data = loc['returned']
         #tmp_data = exec(data, globals(), loc)
